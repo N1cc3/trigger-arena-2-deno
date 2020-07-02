693644af3187bf4f84124c53654680d8c48400bb
Wilmuska <niclaslindgren91@hotmail.com>
2020-07-02 14:45:50 +0300

    Setup Webpack dev server, es6, and card image

---
+++ front/webpack.dev.ts
@@ -0,0 +1,7 @@
+import { Configuration } from 'webpack'
+import merge from 'webpack-merge'
+import base from './webpack.base'
+
+const config: Configuration = merge(base, {
+})
+export default config
